bdb1f32835f5df9b6273619f908eeb56360a0f20
Raphael Dray <dray.raph@gmail.com>
2020-06-27 15:53:17 +0200

    Added second redirection for built in

--- headers/dumsh.h
+++ headers/dumsh.h
@@ -20,12 +20,12 @@
 void dumsh_loop(void);
 int dumsh_num_builtins(void);
 char * dumsh_prompt(void);
-int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size);
-int dumsh_exit(char ** args, int fd, char * stderr, int * stderr_size);
-int dumsh_help(char ** args, int fd, char * stderr, int * stderr_size);
+int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file);
+int dumsh_exit(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file);
+int dumsh_help(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file);
 char * dumsh_read_line(int fd);
 char ** dumsh_split_line(char * line);
-int dumsh_execute(char ** args, char * stderr, int * stderr_size, int fd);
+int dumsh_execute(char ** args, char * stderr, int * stderr_size, int fd, int no_redirect, char * err_file);
 int dumsh_launch(char ** args, int fd, char * stderr, int * stderr_size);
 
 #endif
--- sources/dumsh.c
+++ sources/dumsh.c
@@ -6,7 +6,7 @@ char * builtin_str[] = {
 	"help"
 };
 
-int (*builtin_func[]) (char **, int, char *, int *) = {
+int (*builtin_func[]) (char **, int, char *, int *, int, char *) = {
 	&dumsh_cd,
 	&dumsh_exit,
 	&dumsh_help
@@ -17,6 +17,8 @@ void dumsh_loop(void)
 	char * stderr = malloc(STDERR_BUFF_SIZE * sizeof(char));
 	int stderr_size = STDERR_BUFF_SIZE;
 	int fd = -1;
+	char * filename;
+	char * filename_err = NULL;
 
 	char * line;
 	char ** args;
@@ -33,6 +35,7 @@ void dumsh_loop(void)
 
 		int size = get_size(args);
 		for (int i = 0; i < size; ++i)
+		{
 			if(strcmp(args[i], ">1") == 0 && (i + 1) < size)
 			{
 				is_redirect = 1;
@@ -44,20 +47,40 @@ void dumsh_loop(void)
 				args[i] = NULL;
 				break;
 			}
+			else if(strcmp(args[i], ">2") == 0 && (i + 1) < size)
+			{
+				is_redirect = 2;
+				if (fd < 0)
+					fd = open(args[i + 1], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, S_IRUSR | S_IWUSR);
+				if (fd < 0)
+					perror("open() error");
+				filename = args[i + 1];
+				filename_err = malloc(strlen(filename) + strlen(".err") + 1);
+				strcpy(filename_err, filename);
+				strcat(filename_err, ".err");
+				args[i + 1] = NULL;
+				args[i] = NULL;
+				break;
+			}
+		}
+			
+		if(size > 0 && strcmp(args[0], "exit") == 0)
+			is_redirect = 1;
 
 		status = dumsh_execute(args, stderr, &stderr_size, 
-				(size > 0 && (is_redirect || strcmp(args[0], "exit") == 0)) ? fd : 1);
-		printf("%s\n", stderr);
+							(size > 0 && is_redirect) ? fd : 1, is_redirect, filename_err);
 		prompt = dumsh_prompt();
 	} while (status);
 	close(fd);
 	free(prompt);
 	free(args);
 	free(line);
+	if(filename_err != NULL)
+		free(filename_err);
 	free(stderr);
 }
 
-int dumsh_help(char ** args, int fd, char * stderr, int * stderr_size)
+int dumsh_help(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file)
 {
 	char * lines[] = 
 	{
@@ -103,10 +126,6 @@ int dumsh_launch(char ** args, int fd, char * stderr, int * stderr_size)
 	pid_t pid;
 	int status;
 
-	/*int fds[2] = {0, 0};
-	char * buffer = NULL;
-	size_t buffer_size = 4096;
-	ssize_t bytes_read = 0;*/
 	int fd_temp;
 	if(fd > 2)
 	{
@@ -114,12 +133,6 @@ int dumsh_launch(char ** args, int fd, char * stderr, int * stderr_size)
 		if(fd_temp < 0)
 			perror("open() error");
 	}
-		/*if (pipe(fds) < 0)
-		{
-			perror("pipe() error");
-			return 1;
-		}*/
-
 	
 	pid = fork();
 	if (pid == 0)
@@ -130,9 +143,6 @@ int dumsh_launch(char ** args, int fd, char * stderr, int * stderr_size)
 			close(fd);
 			dup2(fd_temp, STDERR_FILENO);
 			close(fd_temp);
-			/*while ((dup2(fds[1], STDERR_FILENO) == -1) && (errno == EINTR));
-			close(fds[1]);
-			close(fds[0]);*/
 		}
 		if (execvp(args[0], args) == -1)
 			perror("dumsh_launch() error");
@@ -152,61 +162,19 @@ int dumsh_launch(char ** args, int fd, char * stderr, int * stderr_size)
 		{
 			close(fd_temp);
 			return 1;
-			/*close(fds[1]);
-			buffer = malloc(buffer_size);
-			if(!buffer)
-			{
-				perror("malloc() error");
-				return 1;
-			}
-			while (1)
-			{
-				bytes_read = read(fds[0], buffer, buffer_size);
-				if (bytes_read < 0)
-				{
-					if (errno == EINTR)
-						continue;
-					else
-					{
-						perror("read() error");
-						return 1;
-					}
-				}
-				else if (bytes_read == 0)
-					break;
-				else
-				{
-					if (strlen(stderr) + strlen(buffer) >= *stderr_size)
-					{
-						while (strlen(stderr) + strlen(buffer) >= *stderr_size)
-							*stderr_size += STDERR_BUFF_SIZE;
-						stderr = (char *)realloc(stderr, *stderr_size);
-						if(!stderr)
-						{
-							perror("realloc() error");
-							exit(EXIT_FAILURE);
-						}
-					}
-					strcat(stderr, buffer);
-				}
-			}
-			close(fds[0]);
-			printf("%s\n", stderr);
-			free(buffer);
-			//strcpy(stderr, "Coucou");*/
 		}
 	}
 	return 1;
 }
 
-int dumsh_execute(char ** args, char * stderr, int * stderr_size, int fd)
+int dumsh_execute(char ** args, char * stderr, int * stderr_size, int fd, int no_redirect, char * err_file)
 {
 	if (args[0] == NULL)
 		return 1;
 
 	for (int i = 0; i < dumsh_num_builtins(); ++i)
 		if (strcmp(args[0], builtin_str[i]) == 0)
-			return (*builtin_func[i])(args, fd, stderr, stderr_size);
+			return (*builtin_func[i])(args, fd, stderr, stderr_size, no_redirect, err_file);
 	return dumsh_launch(args, fd, stderr, stderr_size);
 }
 
@@ -248,13 +216,13 @@ int dumsh_num_builtins(void)
 	return sizeof(builtin_str) / sizeof(char *);
 }
 
-int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size)
+int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file)
 {
 	if (args[1] == NULL || strcmp(args[1], ">1") == 0)
 	{
 		char * error = "ERR dumsh_cd: cd need at least one argument\n";
 		write(2, error, strlen(error));
-		if (fd > 2) 
+		if (fd > 2 && no_redirect == 1) 
 		{
 			if (strlen(stderr) + strlen(error) >= *stderr_size)
 			{
@@ -270,6 +238,14 @@ int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size)
 			}
 			strcat(stderr, error);
 		}
+		else if(no_redirect == 2 && err_file != NULL)
+		{
+			int fd_temp = open(err_file, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+			if (fd_temp < 0)
+				perror("open() error");
+			write(fd_temp, error, strlen(error));
+			close(fd_temp);
+		}
 	}
 	else if (chdir(args[1]) != 0)
 		perror("dumsh_cd error");
@@ -277,9 +253,9 @@ int dumsh_cd(char ** args, int fd, char * stderr, int * stderr_size)
 	return 1;
 }
 
-int dumsh_exit(char ** args, int fd, char * stderr, int * stderr_size)
+int dumsh_exit(char ** args, int fd, char * stderr, int * stderr_size, int no_redirect, char * err_file)
 {
-	if (fd > 2)
+	if (fd > 2 && no_redirect == 1 && err_file == NULL)
 	{
 		write(fd, "\n", strlen("\n"));
 		for (int i = 0; i < 80; ++i)
